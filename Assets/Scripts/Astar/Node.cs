ac2202aa5899b598b77bc30aa0a9a1e2cecd314b
Timtam2724 <ptfuller@bigpond.com>
2017-05-22 12:02:13 +1000

    Uploading Project

---
+++ Assets/Scripts/Astar/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    // Member variables
+    public bool walkable;
+    public Vector3 position;
+    public int gridX;
+    public int gridZ;
+    public Node parent;
+    public int gCost;
+    public int hCost; // Heuristic
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    // Constructor
+    public Node(bool walkable, Vector3 position, int gridX, int gridZ)
+    {
+        this.walkable = walkable;
+        this.position = position;
+        this.gridX = gridX;
+        this.gridZ = gridZ;
+    }
+}
