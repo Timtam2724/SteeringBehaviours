d0b346590c20460e6643d2292bb5646a0e35fdfb
Timtam2724 <ptfuller@bigpond.com>
2017-06-01 15:59:32 +1000

    Spawner Update

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject prefab; // Prefab of object we want to spawn
+    public float spawnRate = 1f; // Spawn an object every spawn rate interval (in seconds)
+    [HideInInspector]
+    public List<GameObject> objects = new List<GameObject>();
+
+    private float spawnTimer = 0f; // Counts up every frame in seconds
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+    Vector3 GenerateRandomPoint()
+    {
+        // SET halfScale to half of transform' scale
+        Vector3 halfScale = transform.localScale / 2f;
+        // SET randomPoint to zero
+        Vector3 randomPoint = Vector3.zero;
+
+        // SET randomPoint x, y & z to Random Range between
+        randomPoint.x = Random.Range(-halfScale.x, halfScale.x);
+        randomPoint.y = Random.Range(-halfScale.y, halfScale.y);
+        randomPoint.z = Random.Range(-halfScale.z, halfScale.z);
+        // -halfScale to halfScale (Hint: can do individually)
+        // RETURN randomPoint
+        return randomPoint;
+    }
+
+    // Spawns the prefab ta given position and with rotation
+    public void Spawn(Vector3 position, Quaternion rotation)
+    {
+        // SET clone to new instance of prefab
+        GameObject clone = Instantiate(prefab);
+        // ADD clone to objects list
+        objects.Add(clone);
+        // SET clone's position to spawner position + position
+        clone.transform.position = transform.position + position;
+        // SET clone's rotation to rotation
+        clone.transform.rotation = rotation;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        // SET spawnTimer to spawnTimer + delta time
+        spawnTimer += Time.deltaTime;
+        // IF spawnTimer > spawnRate
+        if (spawnTimer > spawnRate)
+        {
+            // SET randomPoint to GenerateRandomPoint()
+            Vector3 randomPoint = GenerateRandomPoint();
+            // CALL Spawn() and pass GenerateRandomPoint(), Quaternion identity
+            Spawn(randomPoint, Quaternion.identity);
+            // SET spawnTimer to zero
+            spawnTimer = 0f;
+        }
+	}
+}
---
+++ Assets/Scripts/Spawner/SpawnerXML.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[RequireComponent(typeof(Spawner))]
+public class SpawnerXML : MonoBehaviour {
+
+    // Contains data for each object spawned
+    public class SpawnerData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+    [XmlRoot]
+    public class XMLContainer
+    {
+        [XmlArray]
+        public Spawner[] data;
+    }
+
+    public string fileName;
+    private Spawner spawner;
+    private string fullPath;
+
+    // Create XMLContainer
+    private XMLContainer xmlContainer;
+
+    // Saves XMLContainer instance to a file as XML format
+    void SaveToPath(string path)
+    {
+        // Create a serializer of type XMLContainer
+        XmlSerializer serializer = new XmlSerializer(typeof(XMLContainer));
+        // Open a file stream at path using Create file mode
+        using (FileStream stream = new FileStream(path, FileMode.Create))
+        {
+            // Serial stream to xmlContainer
+            serializer.Serialize(stream, xmlContainer);
+        }
+    }
+
+    // Loads XMLContainer from path(NOTE: only run if tha file definately exists)
+    XMLContainer Load(string path)
+    {
+        // Create a serializer of type XMLContainer
+        XmlSerializer serializer = new XmlSerializer(typeof(XMLContainer));
+        // Open a file stream at path using Open file mode
+        using (FileStream stream = new FileStream(path, FileMode.Open))
+        {
+            // RETURN the deserialized stream as XML container
+            return serializer.Deserialize(stream) as XMLContainer;
+        }
+    }
+
+    public void Save()
+    {
+        // SET objects to spawner.objects
+        // SET xmlContainer to new XMLContainer
+        // SET xmlContainer.data to new SpawnerData[objects.Count]
+        // FOR i = 0 to objects.Count
+          // SET data to new SpawerData
+          // SET item to object[i]
+          // SET data's position to item's position
+          // SET data's rotaition to items's rotation
+          // SET xmlContainer.data[i] to data
+       // CALL SaveToPath(fullPath)         
+    }
+
+    // Applies the saved data to the scene
+    void Apply()
+    {
+        // SET data to xmlContainer.data
+        // FOR i = 0 to data.Length
+          // SET d to data[i]
+          // CALL spawner.Spawn() and pass d.position, d.rotation
+    }
+
+	// Use this for initialization
+	void Start () {
+		// SET spawner to Spawner Component
+        // SET fullPath to Application.dataPath + "/" + fileName + ".xml"
+        // IF file exists at fullPath
+          // SET xmlContainer to Load(fullPath)
+          // CALL Apply()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
